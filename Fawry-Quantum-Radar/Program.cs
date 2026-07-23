5423e2d3f9468cdc0111daf11a0b563ebb93cc71
menna398 <mennakhaled398@gmail.com>
2026-07-23 16:37:23 +0300

    Add project files.

---
+++ Fawry-Quantum-Radar/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fawry_Quantum_Radar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
