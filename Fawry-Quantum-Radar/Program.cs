d336f3955d736f75f8488250ac804aa9ae66a0d2
menna398 <mennakhaled398@gmail.com>
2026-07-24 09:28:51 +0300

    Implement QuRadar service and application demo

--- Fawry-Quantum-Radar/Program.cs
+++ Fawry-Quantum-Radar/Program.cs
@@ -1,10 +1,107 @@
-﻿namespace Fawry_Quantum_Radar
+﻿using Fawry_Quantum_Radar.Enums;
+using Fawry_Quantum_Radar.Models;
+using Fawry_Quantum_Radar.Rules;
+using Fawry_Quantum_Radar.Services;
+
+namespace Fawry_Quantum_Radar
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Register Rules
+            List<IRule> rules = new()
+            {
+                new SeatbeltRule(),
+                new PrivateSpeedRule(),
+                new TruckSpeedRule()
+            };
+
+            // Create Radar
+            QuRadar radar = new QuRadar(rules);
+
+            // Create Observations
+            List<Observation> observations = new()
+            {
+                new Observation
+                {
+                    PlateNumber = "ABC1234",
+                    Date = DateTime.Now,
+                    CarType = CarType.Private,
+                    Speed = 94,
+                    SeatbeltFastened = false
+                },
+
+                new Observation
+                {
+                    PlateNumber = "EFG5678",
+                    Date = DateTime.Now,
+                    CarType = CarType.Truck,
+                    Speed = 75,
+                    SeatbeltFastened = true
+                },
+
+                new Observation
+                {
+                    PlateNumber = "HIJ9832",
+                    Date = DateTime.Now,
+                    CarType = CarType.Private,
+                    Speed = 70,
+                    SeatbeltFastened = false
+                },
+
+                new Observation
+                {
+                    PlateNumber = "SAFE111",
+                    Date = DateTime.Now,
+                    CarType = CarType.Private,
+                    Speed = 70,
+                    SeatbeltFastened = true
+                }
+            };
+
+            Console.WriteLine("========== Traffic Fines ==========\n");
+
+            foreach (var observation in observations)
+            {
+                Fine? fine = radar.ProcessObservation(observation);
+
+                if (fine == null)
+                    continue;
+
+                Console.WriteLine($"Traffic fine for car {fine.PlateNumber}");
+                Console.WriteLine($"Total amount: {fine.TotalAmount} EGP");
+                Console.WriteLine("Violations:");
+
+                foreach (var violation in fine.Violations)
+                {
+                    Console.WriteLine($"- {violation.Description} : {violation.Fee} EGP");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("====================================");
+            Console.WriteLine("Get All Possible Fines");
+            Console.WriteLine("====================================");
+
+            foreach (var fine in radar.GetAllPossibleFines())
+            {
+                Console.WriteLine($"{fine.PlateNumber} -> {fine.TotalAmount} EGP");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("====================================");
+            Console.WriteLine("Get All Violated Rules Count");
+            Console.WriteLine("====================================");
+
+            foreach (var rule in radar.GetViolatedRulesCount())
+            {
+                Console.WriteLine($"{rule.Key} -> {rule.Value}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Fawry-Quantum-Radar/Services/QuRadar.cs
@@ -0,0 +1,67 @@
+﻿using Fawry_Quantum_Radar.Models;
+using Fawry_Quantum_Radar.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fawry_Quantum_Radar.Services
+{
+    public class QuRadar
+    {
+        private readonly List<IRule> _rules;
+        private readonly List<Fine> _fines = new();
+
+        public QuRadar(List<IRule> rules)
+        {
+            _rules = rules;
+        }
+
+        public Fine? ProcessObservation(Observation observation)
+        {
+            if (observation is null)
+                throw new ArgumentNullException(nameof(observation));
+
+            List<Violation> violations = new();
+
+            foreach (var rule in _rules)
+            {
+                var violation = rule.Check(observation);
+
+                if (violation != null)
+                {
+                    violations.Add(violation);
+                }
+            }
+
+            if (!violations.Any())
+                return null;
+
+            var fine = new Fine
+            {
+                PlateNumber = observation.PlateNumber,
+                Violations = violations
+            };
+
+            _fines.Add(fine);
+
+            return fine;
+        }
+
+        public List<Fine> GetAllPossibleFines()
+        {
+            return _fines;
+        }
+
+        public Dictionary<string, int> GetViolatedRulesCount()
+        {
+            return _fines
+                .SelectMany(f => f.Violations)
+                .GroupBy(v => v.RuleName)
+                .ToDictionary(
+                    group => group.Key,
+                    group => group.Count());
+        }
+    }
+}
